cf664e29a8c416a3abfbdf01d07b56264ddfe947
Brandon Edwards <brandon.edwards144@gmail.com>
2021-08-03 20:46:12 -0400

    Pushing script templates and directories for workshop

---
+++ workshop-script.R
@@ -0,0 +1,61 @@
+####### Script Information ########################
+#' AOS-SCO 2021 bbsBayes Workshop
+#' workshop-script.R
+#' Created August 2021
+#' Last Updated August 2021
+
+####### Script Setup ##############################
+
+
+
+
+####### Chapter 2: Preparation ####################
+
+#' Note from Brandon & Adam: You only need to run 
+#' fetch_bbs_data() once every year when the new version
+#' of the BBS data is released! If you've already run 
+#' fetch_bbs_data(), either when you got the pre-workshop
+#' email, or in previous use of bbsBayes, then you do not
+#' need to run it again here.
+
+
+
+
+
+
+####### Chapter 3: Stratification #################
+
+
+
+
+
+
+####### Chapter 4: Prepare Data ###################
+
+
+
+
+
+
+####### Chapter 5: Modelling ######################
+
+
+
+
+
+
+####### Chapter 6: Indices/Trends #################
+
+
+
+
+
+
+####### Chapter 7: Graphs/Maps ####################
+
+
+
+
+
+
+####### Chapter 8: Advanced Options ###############
